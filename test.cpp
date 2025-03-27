4a9f79520edd53a1d3cf4662e752c503cf556609
dkx20080629 <161914757+dkx20080629@users.noreply.github.com>
2025-03-27 13:03:12 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-//想法 -> bfs -> 每個點都做運算 並賦值給下一層 用dp儲存-> 在bfs遍歷下一層(push到queue之前)時 將下一層的path設為上一層+1
+//想法 -> bfs -> 每個點都做運算 並賦值(上一層的運算結果) 用dp儲存-> 在bfs遍歷下一層(push到queue之前)時 將下一層的path設為上一層+1
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
@@ -9,7 +9,7 @@ int main(){
     cin >> p >> q >> r >> m;
     vector<int> in,path(p+q+r+1,0);//path(這才是dp)用來找最長路徑delay path[i]代表第i個點的長度
     vector<bool> v(p+q+r+1,false);//紀錄queue visited
-    vector<vector<int>> map(p+q+r+1),dp(p+q+r+1); //dp紀錄每個點的狀態 *邏輯閘->dp[邏輯閘種類,訊號1,訊號2] *輸入端口->dp[邏輯閘種類,訊號] *輸出端口->dp[訊號]
+    vector<vector<int>> map(p+q+r+1),dp(p+q+r+1); //dp紀錄每個點的狀態 *邏輯閘->dp[邏輯閘種類,訊號1,訊號2] *輸入端口->dp[5,訊號] *輸出端口->dp[訊號]
     queue<int> que;
     for (int i=0; i<p; i++){
         cin >> a;
@@ -33,34 +33,37 @@ int main(){
         que.pop();
         if (!map[node].empty()){
             if (node>p){//判斷不是輸入端口做計算 遍歷子節點 並把計算結果push到子節點
-                for (int j=0; j<map[node].size(); j++){
+                for (int j:map[node]){
                     if (dp[node][0]==1){//dp[node]後面的 0->種類, 1->訊號1, 2->訊號2 (明確定義在宣告那邊)
                         if (dp[node][1]==1 && dp[node][2]==1){
-                            dp[map[node][j]].push_back(1);
+                            dp[j].push_back(1);
                         }else{
-                            dp[map[node][j]].push_back(0);
+                            dp[j].push_back(0);
                         }
                     }else if (dp[node][0]==2){
                         if (dp[node][1]==1 || dp[node][2]==1){
-                            dp[map[node][j]].push_back(1);
+                            dp[j].push_back(1);
                         }else{
-                            dp[map[node][j]].push_back(0);
+                            dp[j].push_back(0);
                         }
                     }else if (dp[node][0]==3){
                         if (dp[node][1]==dp[node][2]){
-                            dp[map[node][j]].push_back(0);
+                            dp[j].push_back(0);
                         }else{
-                            dp[map[node][j]].push_back(1);
+                            dp[j].push_back(1);
                         }
                     }else if (dp[node][0]==4){
                         if (dp[node][1]==1){
-                            dp[map[node][j]].push_back(0);
+                            dp[j].push_back(0);
                         }else{
-                            dp[map[node][j]].push_back(1);
+                            dp[j].push_back(1);
                         }
+                    }else if (dp[node][0]==5){//輸入端口的下一層 不用計算 把值push給下一層
+                        dp[j].push_back(dp[node][1]);
                     }
                 }
             }else{//輸入端口不用計算 把輸入值push到輸入端口
+                dp[map[node][0]].push_back(5);//之後可以判斷輸入端口的下一層 
                 dp[map[node][0]].push_back(in[node-1]);
             }
         }
