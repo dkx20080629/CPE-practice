8a5e789d0cc6e0eb3c0714606882dff4905cc199
dkx20080629 <161914757+dkx20080629@users.noreply.github.com>
2025-06-02 13:40:51 +0800

    Update 118.cpp

--- 118.cpp
+++ 118.cpp
@@ -2,59 +2,55 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int a, b, n, m,t1,t2;
-    string o,s,to;
-    bool l;
-    set<pair<int,int>> sent;
-    
-    cin >> n >> m;
-    while (cin >> a) {
-        l=false;
-        cin >> b >> o >> s;
+    int maxX, maxY;
+    cin >> maxX >> maxY;
+    set<pair<int, int>> scent;
+    int curX, curY;
+    string orientation, commands;
 
-        for (char move : s) {
-            t1=a;t2=b;to=o;
-            if (move == 'L') {
-                if (o == "N") o = "W";
-                else if (o == "E") o = "N";
-                else if (o == "S") o = "E";
-                else if (o == "W") o = "S";
-            } 
-            else if (move == 'R') {
-                if (o == "N") o = "E";
-                else if (o == "E") o = "S";
-                else if (o == "S") o = "W";
-                else if (o == "W") o = "N";
-            } 
-            else if (move == 'F') {
-                if (o == "N") b++;
-                else if (o == "E") a++;
-                else if (o == "S") b--;
-                else if (o == "W") a--;
+    while (cin >> curX >> curY >> orientation >> commands) {
+        bool isLost = false;
+
+        for (char command : commands) {
+            int prevX = curX, prevY = curY;
+            string prevOrientation = orientation;
+
+            if (command == 'L') {
+                if (orientation == "N") orientation = "W";
+                else if (orientation == "W") orientation = "S";
+                else if (orientation == "S") orientation = "E";
+                else if (orientation == "E") orientation = "N";
+            }else if (command == 'R') {
+                if (orientation == "N") orientation = "E";
+                else if (orientation == "E") orientation = "S";
+                else if (orientation == "S") orientation = "W";
+                else if (orientation == "W") orientation = "N";
+            }else if (command == 'F') {
+                if (orientation == "N") curY++;
+                else if (orientation == "E") curX++;
+                else if (orientation == "S") curY--;
+                else if (orientation == "W") curX--;
             }
-            if (a < 0 || a > n || b < 0 || b > m) {
-                if (sent.count({t1, t2})) {
-                    a = t1;
-                    b = t2;
+
+            if (curX < 0 || curX > maxX || curY < 0 || curY > maxY) {
+                if (scent.count({prevX, prevY})) {
+                    curX = prevX;
+                    curY = prevY;
+                    // Ignore this F move
                 } else {
-                    l = true;
-                    sent.insert({t1,t2});
-                    a = t1;
-                    b = t2;
-                    o = o;
+                    isLost = true;
+                    scent.insert({prevX, prevY});
+                    curX = prevX;
+                    curY = prevY;
                     break;
                 }
             }
         }
-        
-        cout << a << " " << b << " " << o;
-        if (l){
-            cout << " LOST";
-        }
+
+        cout << curX << " " << curY << " " << orientation;
+        if (isLost) cout << " LOST";
         cout << "\n";
     }
+
     return 0;
 }
